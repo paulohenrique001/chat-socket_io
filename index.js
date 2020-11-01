858400c9329d96f0e2ddf500b6b3e6f5b4752647
Paulo Henrique <paulo001.life@gmail.com>
2020-11-01 20:09:36 -0300

    SERVIDOR LISTEN_PROCESS VERSION DEPLOY LAYOUT NOVO

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
+const express = require('express');
+const path = require('path');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'public'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.use('/', (req, res) => {
+    res.render('index.html');
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
+let messages = [];
+
+io.on('connection', socket => {
+    console.log(`Socket conectado: ${socket.id}`);
+
+    socket.emit('previousMessages', messages);
+
+    socket.on('sendMessage', data => {
+        messages.push(data);
+    
+        socket.broadcast.emit('receivedMessage', data);
+    });
 });
 
-http.listen(port, function(){
-  console.log('listening on *:' + port);
-});
+server.listen(process.env.PORT || 3000);
